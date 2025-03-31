647e545fa8810280b95484c1951da751fb27932e
aur0rae <nbadovick@gmail.com>
2025-03-31 17:37:43 -0400

    initial commit

---
+++ software/solar.c
@@ -0,0 +1,29 @@
+/* This is currently just a test program by the book. Expect more
+ * development in the next couple days, and this will eventually turn
+ * into the main program of the solar tracker
+ *
+ * RPi Pico Documentation:
+ * https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+const uint LED_PIN = 25;
+
+int main() {
+    bi_decl(bi_program_description("This is a test binary."));
+    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (1) {
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 1);
+        puts("Hello World\n");
+        sleep_ms(1000);
+    }
+}
