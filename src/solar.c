f0f4c28f718f8a798bdaa3639d456413ec10710f
aur0rae <nbadovick@gmail.com>
2025-04-10 01:45:13 -0400

    Overhauled solar.c to implement basic functionality

--- src/solar.c
+++ src/solar.c
@@ -1,29 +1,75 @@
-/* This is currently just a test program by the book. Expect more
- * development in the next couple days, and this will eventually turn
- * into the main program of the solar tracker
- *
- * RPi Pico Documentation:
- * https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf */
-
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "pico/binary_info.h"
-const uint LED_PIN = 25;
 
-int main() {
-    bi_decl(bi_program_description("This is a test binary."));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+unsigned int servo1 = 0;  /* Servo handling horizontal rotation     */
+unsigned int servo2 = 1;  /* Servo handling vertical positioning    */
+unsigned int inter = 2;   /* Interrupt pin to shut down the program */
+unsigned int led = 25;    /* Built-in LED - no connection           */
 
+/* Function prototypes */
+void gpio_callback(unsigned int inter, uint32_t events);
+void mvservo(unsigned int pos, unsigned int servo);
+void error(void);
+
+/* Main function */
+int main(void) {
+    /* Initialize the GPIO */
     stdio_init_all();
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (1) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
+    /* Create an array to initialize pins cleaner */
+    unsigned int arr[] = {led, servo1, servo2};
+
+    for(unsigned int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        gpio_init(arr[i]);
+        gpio_set_dir(arr[i], GPIO_OUT);
+    }
+
+    /* Initialize the interrupt pin */
+    gpio_set_irq_enabled_with_callback(inter, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+
+    /* Loop to go from position 0 - 180 */
+    while(1) {
+        for(unsigned int i = 0; i < 180; i++) {
+            mvservo(i, servo1);
+        }
+    }
+}
+
+/* Function to handle interrupts */
+void gpio_callback(unsigned int inter, uint32_t events) {
+    error();
+}
+
+/* Function to move a servo to a specific position */
+void mvservo(unsigned int pos, unsigned int servo) {
+    /* Make sure the value passed in is within range 0 - 180 */
+    if(pos <= 180) {
+        /* Scale the ontime from 550us to 2400us based on position variable */
+        unsigned int ontime = ((pos / 3) * 40) + 550;
+
+        /* Send a few pulses to make sure it reads right */
+        for(unsigned int c = 0; c < 5; c++) {
+            /* Set position via duty cycle */
+            gpio_put(servo, 1);
+            sleep_us(ontime);
+
+            gpio_put(servo, 0);
+            sleep_us(20000 - ontime);
+        }
+    }
+
+    else {
+        error();
+    }
+}
+
+/* Function to handle errors and lock up until reboot */
+void error(void) {
+    while(1) {
+        for(unsigned int i = 0; i < 2; i++) {
+            gpio_put(led, i);
+            busy_wait_us_32(500000);
+        }
     }
 }
